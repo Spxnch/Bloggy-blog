6eb323a23553f4602302d056639bc840d860df2a
josejc456 <60475088+josejc456@users.noreply.github.com>
2023-04-15 15:24:07 -0700

    Mis post y header dinamico
    Creacion del menú de "Mis Post" y creacion de un header dinamico para implementarse en las pantallas que sean necesarias

---
+++ html/js/cargarHeader.js
@@ -0,0 +1,16 @@
+document.body.innerHTML = `
+<link rel="stylesheet" href="../../css/estilosHeader.css">
+
+    <header>
+        <div class="logo">
+            <img src="../../assets/images/Group 1.png" alt="logo">
+        </div>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="#">Mi perfil</a></li>
+                <li><a href="#">Cerrar sesion</a></li>
+            </ul>    
+        </nav>
+    </header>
+
+`;
