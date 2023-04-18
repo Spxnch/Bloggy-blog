9705f07ac8b56964caef7a487d89519d301d0228
Alejandra <alejandra.perez216717@potros.itson.edu.mx>
2023-04-18 14:36:02 -0700

    nav

---
+++ js/header.js
@@ -0,0 +1,19 @@
+document.body.innerHTML = `
+    <header>
+    <div class="logo">
+    <img src="../assets/images/Group 1.png" alt="Logo">
+    </div>
+    <div class="menu-btn">
+    <div class="btn-line"></div>
+    <div class="btn-line"></div>
+    <div class="btn-line"></div>
+    </div>
+    <nav class="menu">
+    <ul class="menu-nav">
+        <li class="nav-item"><a href="#" class="nav-link">Inicio de sesion</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Registarte</a></li>
+    </ul>
+    </nav>
+    </header>
+    
+`;
---
+++ js/nav.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu-nav');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('show');
+  menu.classList.toggle('show');
+});
